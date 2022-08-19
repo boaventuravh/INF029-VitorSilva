e514a865a9abcb5fa406d28f86810f518178392e
boaventuravh <111531346+boaventuravh@users.noreply.github.com>
2022-08-19 17:52:16 +0000

    Apliquei a alteração para incluir também os nomes das pessoas

--- Aula001/ExercicioIdades.c
+++ Aula001/ExercicioIdades.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #define QTDE 5
-
+#define QTDE_CHAR 256
 
 int main() {
   int idades[QTDE], media = 0, i, super = 0;
+  char nomes[QTDE][QTDE_CHAR];
   
   for (i=0; i<QTDE; i++)
   {
+    printf("\nDigite o nome da %dª pessoa(maximo de %d caracteres): ", i+1, QTDE_CHAR);
+    fgets(nomes[i], QTDE_CHAR, stdin);
     printf("\nDigite a idade da %dª pessoa : ", i+1);
     scanf("%d", &idades[i]);
+    getchar();   // esse getchar evita que o "\n" seja lido pelo fgets do próximo ciclo no loop
   }
   
   for (i=0; i<QTDE; i++)
@@ -16,16 +20,16 @@ int main() {
 
   media/=QTDE;
 
-  printf ("\nAs idades superiores a media entre os valores fornecidos sao: ");
+  printf ("\nAs pessoas com idades superiores a media entre os valores fornecidos sao: ");
 
   for(i=0;i<QTDE;i++)
   if (idades[i] > media)
   {
-    printf ("\n%d anos", idades[i]);
+    printf ("\n%s com %d anos", nomes[i], idades[i]);
     super += 1;
   }
 
   if (super == 0)
-    printf("\nNao ha idade superiores a media!");
+    printf("\nNao ha pessoas com idade superior a media!");
   return 0;
 }
