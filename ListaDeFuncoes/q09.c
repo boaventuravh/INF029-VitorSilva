a3c8d4fd4bde82a7bf859b1a264f95d1f4f0252e
boaventuravh <111531346+boaventuravh@users.noreply.github.com>
2022-09-07 00:45:33 +0000

    q09 ainda incompleta, mas com avanços

--- ListaDeFuncoes/q09.c
+++ ListaDeFuncoes/q09.c
@@ -14,13 +14,16 @@ masculino; f e F para feminino, o e O para outro).
 
 typedef struct cadastro_de_cliente{
 char nome[257];
-char dataNascimento[11];
+
 char cpf[12];
 char sexo;
 int vNome;
 int vData;
 int vCpf;
 int vSexo;
+struct dataNascimento{
+int dia, mes, ano;
+}Data;
 }Cadastro;
 
 
@@ -28,8 +31,8 @@ int vSexo;
 
 Cadastro cadastrarCliente(Cadastro fCad);
 int valNome(char valiNome[]);
-int valData(char valiData[]);
-
+int valData(int d, int m, int a);
+int valCpf(char valiCpf[]);
 int valSexo(char valiSexo);
 
 int main(){
@@ -39,8 +42,8 @@ int main(){
 
   printf("\nEis os dados do cliente:\n");
   printf("\nNome: %s", mainCad.nome);
-  printf("\nData de nascimento: %s, Validade: %d", mainCad.dataNascimento, mainCad.vData);
-  printf("\nCPF: %s", mainCad.cpf);
+  printf("\nData de nascimento: %d/%d/%d, validade: %d", mainCad.Data.dia, mainCad.Data.mes, mainCad.Data.ano, mainCad.vData);
+  printf("\nCPF: %s, validade: %d", mainCad.cpf, mainCad.vCpf);
   printf("\nSexo: %c, Validade: %d", mainCad.sexo, mainCad.vSexo);
   
   return 0;
@@ -54,21 +57,20 @@ Cadastro cadastrarCliente(Cadastro fCad)
   fgets(fCad.nome, 257, stdin);
   fCad.nome[strlen(fCad.nome) - 1] = '\0';
   printf("\nDigite a data de nascimento no formato dia/mes/ano: ");
-  fgets(fCad.dataNascimento, 11, stdin);
+  scanf("%d/%d/%d", &fCad.Data.dia, &fCad.Data.mes, &fCad.Data.ano);
   getchar();
-  fCad.dataNascimento[strlen(fCad.dataNascimento) - 1] = '\0';
   printf("\nDigite o CPF: ");
   fgets(fCad.cpf, 12, stdin);
   getchar();
-  fCad.cpf[strlen(fCad.cpf) - 1] = '\0';
+  fCad.cpf[strlen(fCad.cpf)] = '\0';
   printf("\nDigite o sexo (M para masculino, F para feminino): ");
   scanf("%c", &fCad.sexo);
 
   //validação dos dados abaixo
   
   fCad.vNome = valNome(fCad.nome);
-  fCad.vData = valData(fCad.dataNascimento);
-
+  fCad.vData = valData(fCad.Data.dia, fCad.Data.mes, fCad.Data.ano);
+  fCad.vCpf = valCpf(fCad.cpf);
   fCad.vSexo = valSexo(fCad.sexo);
   
   return fCad;
@@ -82,12 +84,63 @@ int valNome(char valiNome[])
     return 1;
 }
 
-int valData(char valiData[])
+int valData(int d, int m, int a)
 {
-  if((valiData[0]-48)*10+(valiData[1]-48) > 31 || (valiData[0]-48)*10+(valiData[1]-48) <= 0)
+  if(d<=0 || d>31)
     return -1;
   else
-    return 1;
+  {
+    if(m<=0 || m>12)
+      return -1;
+    else
+    {
+      if(a<=0)
+        return -1;
+      else
+        return 1;
+    }
+  }
+}
+
+int valCpf(char valiCpf[])
+{
+  int i, j, digVeri = 0;
+  for(i=0, j=10;i<9;i++, j--){
+    digVeri+=j*(valiCpf[i]-48);
+    printf("\n%d", digVeri);
+  }
+
+  digVeri = digVeri%11;
+
+  printf("\nVerificador: %d", digVeri);
+    
+  if(digVeri < 2)
+    digVeri = 0;
+  else
+    digVeri = 11-digVeri;
+
+  printf("\nVerificador: %d", digVeri);
+  
+  if(digVeri != valiCpf[9])
+    return -1;
+  else
+  {
+    digVeri = 0;
+    for(i=0, j=11;i<10;i++, j--){
+      digVeri+=j*(valiCpf[i]-48);
+    printf("\n%d", digVeri);
+      
+  }
+    if(digVeri%11 < 2)
+      digVeri = 0;
+    else
+      digVeri = 11 - digVeri%11;
+
+    if(digVeri != valiCpf[10])
+      return -1;
+    else
+      return 1;
+  }
 }
 
 int valSexo(char valiSexo)
