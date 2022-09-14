4114af35b18f759b1538c102199fadff2ee89f4f
boaventuravh <111531346+boaventuravh@users.noreply.github.com>
2022-09-14 17:24:35 -0300

    Quase pronto
    Estou projetando essa função para não permitir que o usuário dê continuidade ao programa enquanto não fornecer um cpf válido.

--- validacaoDeCpf.c
+++ validacaoDeCpf.c
@@ -5,15 +5,21 @@ int validaCpf(char cpf[]);
 
 int main()
 {
-    char cpf[]="00795718241";
-    
-   /* printf("CPF: ");
+    char cpf[20];
+    int validade; //sobre a validade:  se a função retornar -1 o CPF é inválido, se retornar 1, é válido
+    printf("\nCPF (apenas numeros): ");
 	fgets(cpf, 20, stdin);
-	cpf[strcspn(cpf, "\n")] = 0;*/
+	cpf[strcspn(cpf, "\n")] = 0;
+	validade = validaCpf(cpf);
 	
-	validaCpf(cpf);
-	
-    printf("\nEis o CPF digitado: %s", cpf);
+	while(validade == -1)
+	{
+	    fflush(stdin);
+	    printf("\nCPF invalido! Tente novamente: ");
+	    fgets(cpf, 20, stdin);
+	    cpf[strcspn(cpf, "\n")] = 0;
+	    validade = validaCpf(cpf);
+	}
 
     return 0;
 }
@@ -23,15 +29,37 @@ int validaCpf(char cpf[])
     int cpfNum[12];
     int i, j, digiVeri=0;
     
-    for(i=0;cpf[i]!='\0';i++)
-      cpfNum[i] = cpf[i]-48;
+    if(strlen(cpf) != 11)
+        return -1;
     
+    for(i=0;cpf[i]!='\0';i++)
+    {
+      if (cpf[i] < '0' || cpf[i]> '9')  // essa condicional garante que o só entram números no cpf
+        return -1;
+      cpfNum[i] = cpf[i]-48;            // aqui, convertemos o cpf em int pra ficar mais fácil de trabalhar na validação
+    }
     for(i=0,j=10;i<9;i++,j--)
     {
         digiVeri += j*(cpfNum[i]);
-        printf("\nVerificador: %d", digiVeri);
     }
     digiVeri%=11;
     digiVeri = 11 - digiVeri;
-    printf("\nVerificador fora do laco: %d", digiVeri);
+    
+    if (digiVeri != cpfNum[9])
+        return -1;
+    else
+    {
+        digiVeri = 0;
+        for(i=0,j=11;i<10;i++,j--)
+        {
+            digiVeri += j*(cpfNum[i]);
+        }
+        digiVeri%=11;
+        digiVeri = 11 - digiVeri;
+        
+        if (digiVeri != cpfNum[10])
+            return -1;
+        else
+            return 1;
+    }
 }
